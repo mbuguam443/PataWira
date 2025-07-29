e275f1b93fb571f00d959c82266e0b5fd2770e95
Mathew Mbugua <mbuguam443@gmail.com>
2025-07-29 18:01:11 +0300

    push all changes

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('jobdetails',views.jobdetails,name='jobdetails'),
+    path('about',views.about,name='about'),
+    path('joblisting',views.joblisting,name='joblisting'),
+    path('contactus',views.contactus,name='contactus')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+def jobdetails(request):
+    return render(request,'job_details.html')
+def about(request):
+    return render(request,'about.html')
+def joblisting(request):
+    return render(request,'job_listing.html')
+def contactus(request):
+    return render(request,'contact.html')
